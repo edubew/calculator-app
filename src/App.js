602089f5338ca7eede053d17eee5fd9da7319bf0
edubew <edubew98@gmail.com>
2023-03-23 20:15:45 +0300

    Write a markup for the calculator and add basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Calculator from './components/Calculator';
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
 
 export default App;
