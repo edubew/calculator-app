de5c2ca6511d3e5cf8d0a4f4749ea92a810960e8
edubew <edubew98@gmail.com>
2023-03-25 20:38:28 +0300

    Add an error state to api call

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 const Quotes = () => {
   const [data, setData] = useState([]);
-  //   const [loading, setLoading] = useState(false);
-  //   const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    // setLoading(true);
+
     fetch('https://api.api-ninjas.com/v1/quotes?category=funny',
       {
+        // mode: 'no-cors',
         headers: {
           'X-Api-Key': 'hTAKXNFRMzvu2GX5y3pZ0Jo8X9WRprAcTGQV9DPv',
           'Content-Type': 'application/json',
@@ -15,17 +18,28 @@ const Quotes = () => {
       })
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
         setData(data);
-      });
+      })
+      .catch((error) => setError(error.message));
   }, [setData]);
+
+  //   if (loading) return <h2>Loading... please wait</h2>;
+  if (error) {
+    return (
+      <h3>
+        Ooopps!!
+        {' '}
+        {error}
+      </h3>
+    );
+  }
 
   return (
     <div>
       {data.map((item) => (
-        <ul key={item.author}>
+        <ul key={item.author} className="quotes__container">
           <li>{item.quote}</li>
-          <li>{item.author}</li>
+          <li className="author">{item.author}</li>
         </ul>
       ))}
     </div>
