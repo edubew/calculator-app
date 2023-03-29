137c2a9575958839416151840244fef3c44ef6ef
edubew <edubew98@gmail.com>
2023-03-29 11:34:13 +0300

    Create routes for each component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
 
 const App = () => (
-  <div>
-    <Calculator />
-    <Quotes />
-  </div>
+  <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
 );
 
 export default App;
